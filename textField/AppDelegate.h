fce768e2588c53a0ee15b6d82510634cf7384885
Rahul Garg <rahulgarg@Rahuls-MacBook.local>
2014-07-01 17:42:40 +0530

    Initial Commit

---
+++ textField/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  textField
+//
+//  Created by Rahul Garg on 01/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ textFieldTests/textFieldTests.m
@@ -0,0 +1,34 @@
+//
+//  textFieldTests.m
+//  textFieldTests
+//
+//  Created by Rahul Garg on 01/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface textFieldTests : XCTestCase
+
+@end
+
+@implementation textFieldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
